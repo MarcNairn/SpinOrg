023001c67a779ae511f72e6e4e653dca84a8682e
MarcNairn <marcnairn@gmail.com>
2024-07-05 14:23:38 +0200

    Removed unused

--- templates/sample_scripts(templates)/run_comforablydumb.jl
+++
@@ -1,7 +0,0 @@
-include("lib_parallel.jl")
-
-@everywhere include("./parameters_SDe.jl")
-
-sim = run_selfSynch(ps)
-
-save_datal("../data/data_vs_SDe_2.jld2",sim)
--- templates/sample_scripts(templates)/run_job_snowden.jl
+++
@@ -1,7 +0,0 @@
-include("load.jl")
-include("./parameters_phi.jl")
-
-for i in length(enumerate(ps))
-    mycommand = `sbatch run_snowden.sh $i`
-    run(mycommand)
-end
